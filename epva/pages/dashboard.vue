7230bfb93101d1664dfa756caaff1a0be6275f01
sarbend <arjendenizturkmen@gmail.com>
2024-09-29 14:17:07 +0300

    info updates

--- epva/pages/dashboard.vue
+++ epva/pages/dashboard.vue
@@ -32,8 +32,19 @@ const accordionItems = [
   
   <h4>Excel:</h4>
   <ul>
-    <li><a href="https://support.microsoft.com/en-us/office/excel-video-training-9bc05390-e94c-46af-a5b3-d7c22f6990bb" target="_blank">Excel Temelleri</a></li>
-  </ul>` },
+    <li><a href="https://support.microsoft.com/tr-tr/office/excel-video-training-9bc05390-e94c-46af-a5b3-d7c22f6990bb" target="_blank">Excel Temelleri</a></li>
+  </ul>` },{header:"İngilizce Kaynaklar Nasıl Çevirilir?", content:`<p>Chrome'un yerleşik çeviri özelliği, İngilizce kaynakları kolayca Türkçe'ye çevirmenizi sağlar. Bu yöntem, ayrı bir çeviri sayfası açmaya gerek kalmadan, doğrudan web sayfasını çevirmenize olanak tanır. Aşağıdaki stepleri gereksinimleri sağladıktan sonra Opera, Safari, Edge gibi tarayıcılar için de kullanabilirsiniz.</p>
+  
+  <h3>Temel Adımlar:</h3>
+  <ol>
+    <li>Chrome tarayıcısını açın.</li>
+    <li>Çevirmek istediğiniz İngilizce web sayfasına gidin.</li>
+    <li>Chrome otomatik olarak sayfanın dilini algılayacak ve çeviri seçeneği sunacaktır.</li>
+    <li>Çeviri seçeneğine tıklayarak sayfayı Türkçe'ye çevirin.</li>
+  </ol>
+  
+  <p><strong>Not:</strong> Otomatik çevirilerin her zaman mükemmel olamayacağını unutmayın. Özellikle teknik terimlerde veya karmaşık açıklamalarda, orijinal metne de göz atmanız faydalı olabilir.</p>
+  `}
 ]
 </script>
 <style lang="scss" scoped>
