279df8d3a92371e4bd21f58d59eddc8a5bac2ec2
sarbend <arjendenizturkmen@gmail.com>
2024-10-01 11:57:53 +0300

    storyblok initialized

--- epva/nuxt.config.js
+++ epva/nuxt.config.js
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr:false,
-  modules: ['nuxt-vuefire'],
+  modules: ['nuxt-vuefire', ['@storyblok/nuxt', { accessToken: process.env.storyblokKey }]],
   vuefire: {
     config: {
       apiKey: process.env.apiKey,
