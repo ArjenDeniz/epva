af2965cfe1651fdcaa7acdeca15423f52486c575
sarbend <arjendenizturkmen@gmail.com>
2024-10-07 16:09:53 +0300

    button style finalized

--- epva/storyblok/Button.vue
+++ epva/storyblok/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <NuxtLink v-editable="blok" :to="blok.reference" class="button pb-2 pt-2 pr-4 pl-4">
+    <NuxtLink v-editable="blok" :to="blok.reference" class="button pb-2 pt-2 pr-4 pl-4 mb-2 mt-2 ml-2 mr-2">
         {{blok.text}}
     </NuxtLink>
 </template>
@@ -16,7 +16,6 @@ defineProps({ blok: Object })
   border-radius: 4px;
   font-weight: 600;
   transition: background-color 0.3s ease, transform 0.2s ease;
-  margin-right: 10px;
   &:hover {
     background-color: $secondary-color;
     transform: translateY(-2px);
